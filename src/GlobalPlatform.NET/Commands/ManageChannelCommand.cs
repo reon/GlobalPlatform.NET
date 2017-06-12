de40a91daba046b14a20463ee36f34f3687df5fd
James Harling <james@jamesharling.me>
2017-06-12 22:12:19 +0100

    Add MANAGE CHANNEL command support

---
+++ src/GlobalPlatform.NET/Commands/ManageChannelCommand.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using GlobalPlatform.NET.Commands.Abstractions;
+using GlobalPlatform.NET.Commands.Interfaces;
+using GlobalPlatform.NET.Reference;
+
+namespace GlobalPlatform.NET.Commands
+{
+    public interface IChannelOperationPicker
+    {
+        IApduBuilder OpenChannel();
+
+        IChannelPicker CloseChannel();
+    }
+
+    public interface IChannelPicker
+    {
+        IApduBuilder WithIdentifier(byte identifier);
+    }
+
+    public class ManageChannelCommand : CommandBase<ManageChannelCommand, IChannelOperationPicker>,
+        IChannelOperationPicker,
+        IChannelPicker
+    {
+        public IApduBuilder OpenChannel()
+        {
+            this.operation = Operation.Open;
+
+            return this;
+        }
+
+        public IChannelPicker CloseChannel()
+        {
+            this.operation = Operation.Close;
+
+            return this;
+        }
+
+        public IApduBuilder WithIdentifier(byte identifier)
+        {
+            this.identifier = identifier;
+
+            return this;
+        }
+
+        public override IEnumerable<Apdu> Build()
+        {
+            this.p1 = (byte)this.operation;
+            this.p2 = this.operation == Operation.Close ? this.identifier : (byte)0x00;
+
+            yield return Apdu.Build(ApduClass.GlobalPlatform, ApduInstruction.ManageChannel, this.p1, this.p2);
+        }
+
+        private enum Operation : byte
+        {
+            Open = 0x00,
+            Close = 0x80
+        }
+
+        private Operation operation;
+        private byte identifier;
+    }
+}
--- src/GlobalPlatform.NET/ScratchPad.cs
+++ src/GlobalPlatform.NET/ScratchPad.cs
@@ -1,17 +1,19 @@
-﻿using GlobalPlatform.NET.Commands;
-using System.Linq;
+﻿using System.Linq;
+using GlobalPlatform.NET.Commands;
 
 namespace GlobalPlatform.NET
 {
     internal class ScratchPad
     {
+        private static readonly byte[] aid = Enumerable.Range(0, 8).Cast<byte>().ToArray();
+
         public ScratchPad()
         {
             DeleteCommand.Create
                 .WithP1(DeleteCommand.P1.LastOrOnlyCommand)
                 .WithP2(DeleteCommand.P2.DeleteObjectAndRelatedObjects)
                 .DeleteCardContent()
-                .WithAID(Enumerable.Range(0, 8).Cast<byte>().ToArray())
+                .WithAID(aid)
                 .UsingToken(Enumerable.Range(8, 8).Cast<byte>().ToArray())
                 .Build();
 
@@ -21,6 +23,54 @@ namespace GlobalPlatform.NET
                 .DeleteKey()
                 .WithIdentifier(2)
                 .WithVersionNumber(3)
+                .Build();
+
+            GetDataCommand.Create
+                .WithP1P2(GetDataCommand.P1P2.ListApplications)
+                .Build();
+
+            GetStatusCommand.Create
+                .WithP1(GetStatusCommand.P1.IssuerSecurityDomain)
+                .WithP2(GetStatusCommand.P2.GetFirstOrAllOccurrences)
+                .WithNoFilter()
+                .Build();
+
+            GetStatusCommand.Create
+                .WithP1(GetStatusCommand.P1.ExecutableLoadFilesAndModules)
+                .WithP2(GetStatusCommand.P2.GetFirstOrAllOccurrences)
+                .WithFilter(new byte[] { 0xA0, 0x00 })
+                .Build();
+
+            LoadCommand.Create
+                .WithDapBlock(aid, Enumerable.Range(8, 8).Cast<byte>().ToArray())
+                .WithData(new byte[8192])
+                .WithBlockSize(0x80)
+                .Build();
+
+            LoadCommand.Create
+                .WithData(new byte[4096])
+                .WithBlockSize(0xB0)
+                .Build();
+
+            ManageChannelCommand.Create
+                .OpenChannel()
+                .Build();
+
+            ManageChannelCommand.Create
+                .CloseChannel()
+                .WithIdentifier(0x01)
+                .Build();
+
+            SelectCommand.Create
+                .WithP1(SelectCommand.P1.SelectByName)
+                .WithP2(SelectCommand.P2.FirstOrOnlyOccurrence)
+                .SelectIssuerSecurityDomain()
+                .Build();
+
+            SelectCommand.Create
+                .WithP1(SelectCommand.P1.SelectByName)
+                .WithP2(SelectCommand.P2.NextOccurrence)
+                .Select(aid)
                 .Build();
         }
     }
