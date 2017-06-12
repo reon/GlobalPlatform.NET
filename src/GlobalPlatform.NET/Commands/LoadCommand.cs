1660eecc03a7045ce2ead80d9ba87911aaffd7b9
James Harling <james@jamesharling.me>
2017-06-12 22:12:01 +0100

    Code cleanup

--- src/GlobalPlatform.NET/Commands/LoadCommand.cs
+++ src/GlobalPlatform.NET/Commands/LoadCommand.cs
@@ -1,10 +1,9 @@
-﻿using GlobalPlatform.NET.Commands.Abstractions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GlobalPlatform.NET.Commands.Abstractions;
 using GlobalPlatform.NET.Commands.Interfaces;
 using GlobalPlatform.NET.Extensions;
 using GlobalPlatform.NET.Reference;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace GlobalPlatform.NET.Commands
 {
