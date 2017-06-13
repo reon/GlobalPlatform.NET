c014a26db51b20c064924eacd4450d606eb19cc6
James Harling <james@jamesharling.me>
2017-06-13 17:28:06 +0100

    Add tests

---
+++ test/GlobalPlatform.NET.Tests/CommandBuilderTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using GlobalPlatform.NET.Commands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GlobalPlatform.NET.Tests
+{
+    [TestClass]
+    public class CommandBuilderTests
+    {
+        private static readonly byte[] aid = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+        private static IEnumerable<Apdu> apdus;
+
+        [TestMethod]
+        public void Select()
+        {
+            apdus = SelectCommand.Create
+                .WithP1(SelectCommand.P1.SelectByName)
+                .WithP2(SelectCommand.P2.FirstOrOnlyOccurrence)
+                .SelectIssuerSecurityDomain()
+                .Build();
+
+            apdus.First().Buffer.Should().BeEquivalentTo(new byte[] { 0x00, 0xA4, 0x04, 0x00, 0x00 });
+
+            apdus = SelectCommand.Create
+                .WithP1(SelectCommand.P1.SelectByName)
+                .WithP2(SelectCommand.P2.NextOccurrence)
+                .Select(aid)
+                .Build();
+
+            apdus.First().Buffer.Should().BeEquivalentTo(new byte[] { 0x00, 0xA4, 0x04, 0x02, 0x08, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00 });
+        }
+    }
+}
---
+++ test/GlobalPlatform.NET.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GlobalPlatform.NET.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GlobalPlatform.NET.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a00a5242-b9cf-49a1-a25e-1ae618305a49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
