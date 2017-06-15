a981d2037cabb5817f906afb23deb5866040033c
James Harling <james@jamesharling.me>
2017-06-15 19:07:07 +0100

    Deprecate IP1P2Picker interface

--- src/GlobalPlatform.NET/Commands/GetDataCommand.cs
+++ src/GlobalPlatform.NET/Commands/GetDataCommand.cs
@@ -1,54 +1,69 @@
-﻿using GlobalPlatform.NET.Commands.Interfaces;
-using GlobalPlatform.NET.Reference;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using GlobalPlatform.NET.Commands.Abstractions;
+using GlobalPlatform.NET.Commands.Interfaces;
+using GlobalPlatform.NET.Reference;
 
 namespace GlobalPlatform.NET.Commands
 {
-    public class GetDataCommand : IP1P2Picker<GetDataCommand.P1P2, IApduBuilder>,
-        IApduBuilder
+    public enum DataObject : ushort
     {
-        private P1P2 p1p2;
+        IssuerIdentificationNumber = 0x0042,
+        CardImageNumber = 0x0045,
+        CardData = 0x0066,
+        KeyInformationTemplate = 0x00E0,
+        CardCapabilityInformation = 0x0067,
+        CurrentSecurityLevel = 0x00D3,
+        ListApplications = 0x2F00,
+        ExtendedCardResourcesInformation = 0xFF21,
+        SecurityDomainManagerUrl = 0x5F50
+    }
 
-        public enum P1P2 : ushort
+    public interface IGetDataObjectPicker
+    {
+        IGetDataTagListPicker GetDataFrom(DataObject getDataObject);
+    }
+
+    public interface IGetDataTagListPicker : IApduBuilder
+    {
+        IApduBuilder WithTagList(params byte[] tags);
+    }
+
+    public class GetDataCommand : CommandBase<GetDataCommand, IGetDataObjectPicker>,
+        IGetDataObjectPicker,
+        IGetDataTagListPicker
+    {
+        private DataObject getDataObject;
+        private byte[] tagList = new byte[0];
+
+        public IGetDataTagListPicker GetDataFrom(DataObject getDataObject)
         {
-            IssuerIdentificationNumber = 0x0042,
-            CardImageNumber = 0x0045,
-            CardData = 0x0066,
-            KeyInformationTemplate = 0x00E0,
-            CardCapabilityInformation = 0x0067,
-            CurrentSecurityLevel = 0x00D3,
-            ListApplications = 0x2F00,
-            ExtendedCardResourcesInformation = 0xFF21,
-            SecurityDomainManagerUrl = 0x5F50
+            this.getDataObject = getDataObject;
+
+            return this;
         }
 
-        /// <summary>
-        /// Starts building the command. 
-        /// </summary>
-        public static IP1P2Picker<P1P2, IApduBuilder> Create => new GetDataCommand();
-
-        public IEnumerable<Apdu> AsApdu()
+        public IApduBuilder WithTagList(params byte[] tagList)
         {
-            var bytes = BitConverter.GetBytes((ushort)this.p1p2);
+            this.tagList = tagList;
+
+            return this;
+        }
+
+        public override IEnumerable<Apdu> AsApdu()
+        {
+            var bytes = BitConverter.GetBytes((ushort)this.getDataObject);
 
             if (BitConverter.IsLittleEndian)
             {
-                bytes.Reverse();
+                bytes = bytes.Reverse().ToArray();
             }
 
             byte p1 = bytes.First();
             byte p2 = bytes.Last();
 
-            yield return Apdu.Build(ApduClass.GlobalPlatform, ApduInstruction.GetData, p1, p2);
-        }
-
-        public IApduBuilder WithP1P2(P1P2 p1p2)
-        {
-            this.p1p2 = p1p2;
-
-            return this;
+            yield return Apdu.Build(ApduClass.GlobalPlatform, ApduInstruction.GetData, p1, p2, this.tagList);
         }
     }
 }
--- src/GlobalPlatform.NET/Commands/Interfaces/IP1P2Picker.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GlobalPlatform.NET.Commands.Interfaces
-{
-    public interface IP1P2Picker<TP1P2, TBuilder>
-    {
-        TBuilder WithP1P2(TP1P2 p1p2);
-    }
-}
