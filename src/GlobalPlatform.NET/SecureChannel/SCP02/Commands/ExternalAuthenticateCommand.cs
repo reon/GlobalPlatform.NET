fd2a1b62a765e57c27832cab6957269db3e6390e
James Harling <james@jamesharling.me>
2017-06-27 22:08:18 +0100

    Continue addition of SCP02 support

---
+++ src/GlobalPlatform.NET/SecureChannel/SCP02/Commands/ExternalAuthenticateCommand.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using GlobalPlatform.NET.Commands.Abstractions;
+using GlobalPlatform.NET.Commands.Interfaces;
+using GlobalPlatform.NET.Reference;
+using GlobalPlatform.NET.SecureChannel.SCP02.Reference;
+
+namespace GlobalPlatform.NET.SecureChannel.SCP02.Commands
+{
+    public interface ISecurityLevelPicker
+    {
+        IHostChallengePicker WithSecurityLevel(SecurityLevel securityLevel);
+    }
+
+    public interface IHostChallengePicker
+    {
+        ISecureChannelKeyPicker UsingHostChallenge(byte[] hostChallenge);
+    }
+
+    public interface ISecureChannelKeyPicker
+    {
+        IApduBuilder AndKey(byte[] key);
+    }
+
+    public class ExternalAuthenticateCommand : CommandBase<ExternalAuthenticateCommand, ISecurityLevelPicker>,
+        ISecurityLevelPicker,
+        IHostChallengePicker,
+        ISecureChannelKeyPicker
+    {
+        private byte[] hostChallenge;
+        private byte[] key;
+        private byte[] hostCryptogram;
+
+        public IHostChallengePicker WithSecurityLevel(SecurityLevel securityLevel)
+        {
+            this.P1 = (byte)securityLevel;
+
+            return this;
+        }
+
+        public ISecureChannelKeyPicker UsingHostChallenge(byte[] hostChallenge)
+        {
+            this.hostChallenge = hostChallenge;
+
+            return this;
+        }
+
+        public IApduBuilder AndKey(byte[] key)
+        {
+            this.key = key;
+
+            return this;
+        }
+
+        public override IEnumerable<Apdu> AsApdu()
+        {
+            yield return Apdu.Build(ApduClass.GlobalPlatform, ApduInstruction.ExternalAuthenticate, this.P1, this.P2, this.hostCryptogram);
+        }
+    }
+}
---
+++ src/GlobalPlatform.NET/SecureChannel/SCP02/Reference/SecurityLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace GlobalPlatform.NET.SecureChannel.SCP02.Reference
+{
+    public enum SecurityLevel : byte
+    {
+        Authenticated = 0b00000000,
+        CMac = 0b00000001,
+        CDecryption = 0b00000011,
+        RMac = 0b00010000,
+        CMacRMac = 0b00010001,
+        CDecryptionCMacRMac = 0b00010011
+    }
+}
